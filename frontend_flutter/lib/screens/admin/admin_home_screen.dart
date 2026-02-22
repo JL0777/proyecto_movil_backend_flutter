b54203b834c505087d6a6576118b3e78af3afd92
07 <josellm2004@hotmail.com>
2026-02-22 11:42:17 -0500

    chore: logout logic admin/kithen

--- frontend_flutter/lib/screens/admin/admin_home_screen.dart
+++ frontend_flutter/lib/screens/admin/admin_home_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import '../../core/session/session_manager.dart';
+import '../login_screen.dart';
 
 class AdminHomeScreen extends StatelessWidget {
   final Map<String, dynamic> user;
 
-  const AdminHomeScreen({super.key, required this.user});
+  const AdminHomeScreen({
+    super.key,
+    required this.user,
+  });
+
+  Future<void> _logout(BuildContext context) async {
+    await SessionManager.clearSession();
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const LoginScreen(),
+      ),
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +28,12 @@ class AdminHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Panel Administrador"),
         backgroundColor: Colors.orange,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context),
+          ),
+        ],
       ),
       body: Center(
         child: Text(
--- frontend_flutter/lib/screens/cocina/cocina_home_screen.dart
+++ frontend_flutter/lib/screens/cocina/cocina_home_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import '../../core/session/session_manager.dart';
+import '../login_screen.dart';
 
 class CocinaHomeScreen extends StatelessWidget {
   final Map<String, dynamic> user;
 
-  const CocinaHomeScreen({super.key, required this.user});
+  const CocinaHomeScreen({
+    super.key,
+    required this.user,
+  });
+
+  Future<void> _logout(BuildContext context) async {
+    await SessionManager.clearSession();
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const LoginScreen(),
+      ),
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +28,12 @@ class CocinaHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Panel Cocina"),
         backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context),
+          ),
+        ],
       ),
       body: Center(
         child: Text(
